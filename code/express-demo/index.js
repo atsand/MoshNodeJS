a52df0eb57366919e621a9a4ca46609ee82d7f13
Aaron Sandusky <atsand23@gmail.com>
2021-11-21 17:23:03 -0500

    create put and delete endpoints

--- code/express-demo/index.js
+++ code/express-demo/index.js
@@ -1,5 +1,6 @@
 const Joi = require('joi');
 const express = require('express');
+const { response } = require('express');
 const app = express();
 
 app.use(express.json());
@@ -25,17 +26,8 @@ app.get('/api/courses', (req, res) => {
 
 //post request
 app.post('/api/courses', (req, res) => {
-  //create Joi schema to validate request body object
-  const schema = Joi.object({
-    name: Joi.string().min(3).required(),
-  });
-
-  const result = schema.validate(req.body);
-  //bad request if no name
-  if (result.error) {
-    res.status(400).send(result.error.details[0].message);
-    return;
-  }
+  const { error } = validateCourse(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
   const course = {
     id: courses.length + 1,
@@ -45,11 +37,26 @@ app.post('/api/courses', (req, res) => {
   res.send(course);
 });
 
+app.put('/api/courses/:id', (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    return res.status(404).send('The course with the given ID was not found');
+
+  // const result = validateCourse(course);
+  //using object destructuring to just pull a prop of a return object
+  const { error } = validateCourse(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  course.name = req.body.name;
+  res.send(course);
+});
+
 //send a parameter
 app.get('/api/courses/:id', (req, res) => {
   const course = courses.find((c) => c.id === parseInt(req.params.id));
   if (!course)
-    res.status(404).send('The course with the given ID was not found');
+    return res.status(404).send('The course with the given ID was not found');
+
   res.send(course);
 });
 
@@ -62,6 +69,25 @@ app.get('/api/posts/:year/:month', (req, res) => {
   //req url /api/posts/2018/3?sortBy=myparam
   res.send(req.query.sortBy);
 });
+
+app.delete('/api/courses/:id', (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    return res.status(404).send('The course with the given ID was not found');
+
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+
+  res.send(course);
+});
+
+function validateCourse(course) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+
+  return schema.validate(course);
+}
 
 //PORT - environment variable
 //look for environment variable first, then assign if not there
