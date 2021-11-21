5e0c7d0b9c5c9d8f3bde23d9171fdaee5231bd5f
Aaron Sandusky <atsand23@gmail.com>
2021-11-21 17:31:45 -0500

    Merge branch 'develop' into EventEmitter
    # Conflicts:
    #	code/first-app/app.js

---
+++ code/express-demo/index.js
@@ -0,0 +1,95 @@
+const Joi = require('joi');
+const express = require('express');
+const { response } = require('express');
+const app = express();
+
+app.use(express.json());
+
+const courses = [
+  {
+    id: 1,
+    name: 'course1',
+  },
+  {
+    id: 2,
+    name: 'course2',
+  },
+];
+
+app.get('/', (req, res) => {
+  res.send('Hello World!!');
+});
+
+app.get('/api/courses', (req, res) => {
+  res.send(courses);
+});
+
+//post request
+app.post('/api/courses', (req, res) => {
+  const { error } = validateCourse(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(course);
+  res.send(course);
+});
+
+app.put('/api/courses/:id', (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    return res.status(404).send('The course with the given ID was not found');
+
+  // const result = validateCourse(course);
+  //using object destructuring to just pull a prop of a return object
+  const { error } = validateCourse(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  course.name = req.body.name;
+  res.send(course);
+});
+
+//send a parameter
+app.get('/api/courses/:id', (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    return res.status(404).send('The course with the given ID was not found');
+
+  res.send(course);
+});
+
+//send multiple parameters
+app.get('/api/posts/:year/:month', (req, res) => {
+  //access param properties
+  // res.send(req.params);
+
+  //access query params
+  //req url /api/posts/2018/3?sortBy=myparam
+  res.send(req.query.sortBy);
+});
+
+app.delete('/api/courses/:id', (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    return res.status(404).send('The course with the given ID was not found');
+
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+
+  res.send(course);
+});
+
+function validateCourse(course) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+
+  return schema.validate(course);
+}
+
+//PORT - environment variable
+//look for environment variable first, then assign if not there
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`listening on port ${port}...`));
