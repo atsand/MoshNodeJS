c2ddd63f0311c3f486faebd18c02d7fb31e7c464
Aaron Sandusky <atsand23@gmail.com>
2021-11-30 22:13:01 -0500

    use promises to replace callbacks and named functions

--- code/async-demo/index.js
+++ code/async-demo/index.js
@@ -1,36 +1,43 @@
 console.log('Before');
-getUser(1, getRepositoriesByUser);
+// getUser(1, (user) => {
+//   getRepositories(user.gitHubUserName, (repos) => {
+//     getCommits(repos[0], (commits) => {
+//       console.log(commits);
+//     });
+//   });
+// });
+
+getUser(1)
+  .then((user) => getRepositories(user.gitHubUserName))
+  .then((repos) => getCommits(repos[0]))
+  .then((commits) => console.log('Commits', commits))
+  .catch((err) => console.log('Error', err.message));
+
 console.log('After');
 
-function getRepositoriesByUser(user) {
-  getRepositories(user.gitHubUserName, getCommitsForRepo);
+function getUser(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('Getting user from db...');
+      resolve({ id: id, gitHubUserName: 'Aaron' });
+    }, 2000);
+  });
 }
 
-function getCommitsForRepo(repos) {
-  getCommits(repos[0], displayCommits);
+function getRepositories(username) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('Getting repositories from gitHub...');
+      resolve(['repo1', 'repo2', 'repo3']);
+    }, 2000);
+  });
 }
 
-function displayCommits(commits) {
-  console.log(commits);
-}
-
-function getUser(id, callback) {
-  setTimeout(() => {
-    console.log('Getting user from db...');
-    callback({ id: id, gitHubUserName: 'Aaron' });
-  }, 2000);
-}
-
-function getRepositories(username, callback) {
-  setTimeout(() => {
-    console.log('Getting repositories from gitHub...');
-    callback(['repo1', 'repo2', 'repo3']);
-  }, 2000);
-}
-
-function getCommits(repo, callback) {
-  setTimeout(() => {
-    console.log('Getting commits for repo...');
-    callback(['commit1', 'commit2', 'commit4']);
-  }, 2000);
+function getCommits(repo) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('Getting commits from gitHub...');
+      resolve(['commit1', 'commit2', 'commit3']);
+    }, 2000);
+  });
 }
